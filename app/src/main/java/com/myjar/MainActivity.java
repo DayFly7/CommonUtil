2c252c24a4d0068b8b2e65209b61841c9fba6032
DayFly7 <qyang122@126.com>
2018-04-04 11:17:27 +0800

    Rejected commit: Default

--- app/src/main/java/com/myjar/MainActivity.java
+++ app/src/main/java/com/myjar/MainActivity.java
@@ -2,12 +2,31 @@ package com.myjar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.myjar.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void initView() {
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start(SecondActivity.class, new OnBundleListener() {
+                    @Override
+                    public Bundle onSetBundle(Bundle bundle) {
+                        bundle.putString("qq","qqqqqq");
+                        return bundle;
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+
     }
 }
