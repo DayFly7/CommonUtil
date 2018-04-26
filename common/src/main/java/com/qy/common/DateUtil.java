6c31f7a65417ee9fdeb4896a06c7fd3dcbf7b466
DayFly7 <qyang122@126.com>
2018-04-26 14:59:31 +0800

    Rejected commit: Default

--- common/src/main/java/com/qy/common/DateUtil.java
+++ common/src/main/java/com/qy/common/DateUtil.java
@@ -8,42 +8,45 @@ import java.util.Date;
  * Created by yangqi on 17/7/31.
  */
 public class DateUtil {
-    public static String getCurrentDate(){
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    public static final String FORMAT_TYPE_DATE = "yyyy-MM-dd";
+    public static final String FORMAT_TYPE_DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+
+    public static String getCurrentDate(String formatType){
+        SimpleDateFormat formatter = new SimpleDateFormat(formatType);
         Date curDate = new Date(System.currentTimeMillis());
         String str = formatter.format(curDate);
         return str;
     }
 
-    public static String getCurrentTime(){
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+    public static String getCurrentTime(String formatType){
+        SimpleDateFormat formatter = new SimpleDateFormat(formatType);
         Date curDate = new Date(System.currentTimeMillis());
         String str = formatter.format(curDate);
         return str;
     }
 
-    public static String dateToString(Date date){
+    public static String dateToString(Date date,String formatType){
         if (null == date){
             return "";
         }
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat formatter = new SimpleDateFormat(formatType);
         String str = formatter.format(date);
         return str;
     }
 
-    public static String getStrDate(String timeStamp){
+    public static String getStrDate(String timeStamp,String formatType){
         if (timeStamp.length() == 0){
             return "";
         }
         String timeString = null;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat(formatType);
         long  l = Long.valueOf(timeStamp);
         timeString = sdf.format(new Date(l));//单位秒
         return timeString;
     }
 
-    public static long getLongDate(String time) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    public static long getLongDate(String time,String formatType) {
+        SimpleDateFormat sdf = new SimpleDateFormat(formatType);
         Date date = new Date();
         try {
             date = sdf.parse(time);
@@ -52,6 +55,7 @@ public class DateUtil {
         }
         return date.getTime();
     }
+
 
     public static String getTwoPositionTime(int str){
         String result = String.valueOf(str);
