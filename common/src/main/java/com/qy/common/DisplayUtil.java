f9be9fb45090093a490eba9673125acd298b5b6c
DayFly7 <qyang122@126.com>
2018-04-26 09:16:16 +0800

    Rejected commit: Default

--- common/src/main/java/com/qy/common/DisplayUtil.java
+++ common/src/main/java/com/qy/common/DisplayUtil.java
@@ -20,4 +20,8 @@ public class DisplayUtil {
     public static int getWidth(Context context){
         return context.getResources().getDisplayMetrics().widthPixels;
     }
+
+    public static int getHeight(Context context){
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
 }
